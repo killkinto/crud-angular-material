484e538ed6eb4eeff6938ee8f6e5eb3e7f439a6c
Fernando <killkinto@gmail.com>
2025-09-16 14:55:46 -0300

    Implementação do cadastro

---
+++ .angular/cache/19.2.16/crud-angular-material/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-7AQ3RK2W.js";
+import "./chunk-256YEQI2.js";
+import "./chunk-4GQNXL7P.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [
+    FlexLayoutModule, 
+    MatCardModule, 
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent {
+
+}
